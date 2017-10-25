c020ea8d27a1882727d816c4ef0d7a7c8805367b
exymax <exymax@gmail.com>
2017-10-25 14:41:11 +0300

    Added header styling and animation on scroll

--- js/es6/app.js
+++ js/es6/app.js
@@ -3,9 +3,19 @@ import Rellax from 'rellax';
 import '../../styles/app.less';
 
 document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('#header');
+
     const parallax = new Rellax('.parallax', {
         speed: -10,
         center: false,
         round: true,
+    });
+
+    window.addEventListener('scroll', (e) => {
+        if (window.scrollY > 20) {
+            header.classList.add('colored');
+        } else if (header.classList.contains('colored')) {
+            header.classList.remove('colored');
+        }
     });
 });
