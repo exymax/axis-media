6d13f43aa5e862fed159bdc77bb5ec3a8799240a
exymax <exymax@gmail.com>
2017-10-26 12:29:21 +0300

    Added directions block & modified logo image animation on scroll

--- js/es6/app.js
+++ js/es6/app.js
@@ -4,6 +4,7 @@ import '../../styles/app.less';
 
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('#header');
+    const logoImage = document.querySelector('#logo img');
 
     const parallax = new Rellax('.parallax', {
         speed: -10,
@@ -14,8 +15,10 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', (e) => {
         if (window.scrollY > 20) {
             header.classList.add('colored');
+            logoImage.src= 'images/logo-dark.png';
         } else if (header.classList.contains('colored')) {
             header.classList.remove('colored');
+            logoImage.src= 'images/logo.png';
         }
     });
 });
